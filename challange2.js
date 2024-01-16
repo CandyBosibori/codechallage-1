ddc89d013e3514de37da670289cc7a743a3e5d28
Candy <candy.ongaro@student.moringaschool.com>
2024-01-16 21:27:16 +0300

    complete assigmnet

---
+++ challange2.js
@@ -0,0 +1,24 @@
+let speedLimit = 70;
+let demeritPointsPer5km = 1;
+
+//Function to calculate the Demerit points
+
+
+function detectSpeed(speed){
+ // checking and calculating the demerit
+    if(speed<speedLimit){
+        console.log('Ok');
+    }else{
+        let excess_speed = speed - speedLimit;
+        demeritPointsPer5km = excess_speed / 5;
+
+        console.log(`Points: ${demeritPointsPer5km}`);
+    }
+// Checking if demerits have surpassed the limit
+    if(demeritPointsPer5km > 12){
+        console.log('License suspended')
+    }
+    return speed
+ }
+// calling the function and passing an argument
+ detectSpeed(95)
